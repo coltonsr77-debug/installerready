78ffe07c6842ae16cc1c5e5c99ec4aa54923e89f
coltonsr77 <coltonr3@icloud.com>
2025-10-25 20:48:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="InstallerReady",
-    version="0.5",
+    name="installerready",
+    version="0.6",
     author="coltonsr77",
-    author_email="coltonsr77@gmail.com",  # replace with your email
-    description="A simple Tkinter GitHub project installer",
+    description="A Tkinter GitHub project installer",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/coltonsr77/installerready",
-    project_urls={
-        "Bug Tracker": "https://github.com/coltonsr77/InstallerReady/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: Microsoft :: Windows",
-        "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: MIT License",
-    ],
-    license="MIT",
+    url="https://github.com/coltonsr77/InstallerReady",
     packages=find_packages(),
     install_requires=["requests"],
     python_requires=">=3.10",
