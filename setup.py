ac43638c1002c8ec0d98ea70cfdd70f6dee0bc9c
coltonsr77 <coltonr3@icloud.com>
2025-10-26 16:56:50 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="InstallerReady",
-    version="0.7",
+    version="0.8",
     author="coltonsr77",
     description="A Tkinter GitHub project installer",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/coltonsr77/ installerready",
     packages=find_packages(),
-    install_requires=["requests"],
-    python_requires=">=3.9",
+    install_requires=["requests", "kitecmd-IR"],
+    python_requires=">=3.8",
     entry_points={
         "gui_scripts": [
             "installerready=installerready.app:main",
